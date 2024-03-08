1faed1ce53e8b539e80a2efcfe05b6aa79ee732c
Rafael Lopes <125132682+rafxnzz@users.noreply.github.com>
2024-03-07 21:22:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Trabalho de PW2</title>
 </head>
 <body>
     <?php
-    /* Laço
-    for($i=0; $i<=10; $i++){
-            echo $i;
+        if(isset($_GET['operacao']) && isset ($_GET['x']) && isset ($_GET['y'])) {
+            if($_GET ['operacao'] == "adicao"){
+                echo $_GET['x'] + $_GET['y'];
+            }
+            else if($_GET ['operacao'] == "subtracao"){
+                echo $_GET['x'] - $_GET['y'];
+            }
+            else if($_GET ['operacao'] == "multiplicacao"){
+                echo $_GET['x'] * $_GET['y'];
+            }
+            else if($_GET ['operacao'] == "divisao"){
+                echo $_GET['x'] / $_GET['y'];
+            }
+            else {
+                echo "erro";
+            }
         }
-    */
-    /*
-    echo $_GET ['nome'] .  "<br>";
-        echo $_GET['cor'] . "<br>";
-        echo $_GET['musica'];
-    */
-    /*
-    if($_GET ['pagina'] == "home"){
-        echo "Página Home";
-    }
-    else if ($_GET ['pagina'] == "cadastrar") {
-        echo "Página Cadastrar";
-    }
-    else {
-        echo "Página X";
-    }
-    */
-    if(isset($_GET['operacao']) && isset ($_GET['x']) && isset ($_GET['y'])) {
-    if($_GET ['operacao'] == "adicao"){
-        echo $_GET['x'] + $_GET['y'];
-    }
-    else if($_GET ['operacao'] == "subtracao"){
-        echo $_GET['x'] - $_GET['y'];
-    }
-    else if($_GET ['operacao'] == "multiplicacao"){
-        echo $_GET['x'] * $_GET['y'];
-    }
-    else if($_GET ['operacao'] == "divisao"){
-        echo $_GET['x'] / $_GET['y'];
-    }
-    else {
-        echo "erro";
-    }
-    }
     ?>
 </body>
 </html>
